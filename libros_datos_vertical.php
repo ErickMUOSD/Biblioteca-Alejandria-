7d767fc7ca76d9ba90a7746cdb7ecc84672178cf
ErickMUOSD <martinoli944@gmail.com>
2021-07-21 23:52:43 -0500

    change post for get

--- libros_datos_vertical.php
+++ libros_datos_vertical.php
@@ -2,7 +2,7 @@
 require_once './conexion.php';
     $sql = 'select * from libros where id_libro = :id ';
     $sentencia = $conexion->prepare($sql);
-    $sentencia->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
+    $sentencia->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
     $sentencia->execute();
     echo json_encode([
         'estatus' => true, 'data' => $sentencia->fetchAll(PDO::FETCH_ASSOC)
