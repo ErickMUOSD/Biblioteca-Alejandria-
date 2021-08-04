84fbadfcbb9d9afba7bc6e85e8c23f87b0798d40
2021 DSM33 Valeria Yamin Anastasio Mejia <al222010297@gmail.com>
2021-08-03 21:54:41 -0500

    Archivos corregidos en navegación

--- categorias.php
+++ categorias.php
@@ -6,13 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="css/nav-bar.css">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/nav-bar.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/nav-bar.css">
-    <script defer src="bootstrap/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/nav-bar.css">
+    <script defer src="js/bootstrap.min.js"></script>
+    
     <title>Categorías</title>
 </head>
 <body>
