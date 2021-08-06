a9d14c308d16f0092d3f713916955c9fb003f081
2021 DSM33 Perez Cuadros Victor Eduardo <al222010016@gmail.com>
2021-08-05 22:45:55 -0500

    Solucion

--- categorias.php
+++ categorias.php
@@ -14,6 +14,22 @@
 </head>
 <body>
 
+<!DOCTYPE html>
+<html lang="es-Mx">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/nav-bar.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <script defer src="js/bootstrap.min.js"></script>
+    
+    <title>Categorías</title>
+</head>
+<body style="background-color: #F2F7FF;">
+
 <?php
     require_once('./framgents-html/navegacion_admin.html');
 ?>
@@ -21,11 +37,13 @@
   
 
     <!-- Tabla -->
-   
-    <table class="table table-hover table-bordered table-sm">
+   <div class="container-fluid m-3" >
+   <div class='card'>
+   <div class='card-body'>
+    <table class="table table-striped table-hover">
         <thead>
             <tr>
-                <th style="width: 20%;"scope="col">Id Categoría</th>
+               
                 <th style="width: 40%;"scope="col">Nombre de la Categoría</th>
                 <th style="width: 20%;"scope="col">Estatus de la Categoría</th> 
                 <th style="width: 20%;">
@@ -37,7 +55,7 @@
                 </th>
             </tr>
         </thead>
-        
+    
         <?php
                                 require_once './conexion.php';
                                 $sql = 'select id_categoria, nombre_categoria, estatus_categoria from categorias order by nombre_categoria asc';
@@ -58,7 +76,9 @@
 fin;
                                 }
                                 ?>
-
+</div>
+        </div>
+     </div>
       
 </body>
 </html>
