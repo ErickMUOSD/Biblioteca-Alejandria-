64310e84eeb2c83c041b0814230e25aa7c6f3ee6
ErickMUOSD <martinoli944@gmail.com>
2021-08-10 13:29:03 -0500

    refactor design

--- editoriales.php
+++ editoriales.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="es-Mx">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/nav-bar.css">
+    <link rel="stylesheet" href="css/editoriales.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script defer src="js/bootstrap.min.js"></script>
     <title>Editoriales</title>
 </head>
-<body>
-<?php
+
+<body style="background-color: #F2F7FF">
+    <?php
     require_once('./framgents-html/navegacion_admin.html');
 
     ?>
@@ -21,22 +24,23 @@
     <div class='container mt-3'>
         <div class='row'>
             <div class='col-3'></div>
-            <div class='col-6'>
-                    <div class='card'>
-                        <div class='card-header'>
-                        <i class="bi bi-book-half"></i>Editoriales
-                        </div>
-                        <div class='card-body'>
-                            <a class="float-end btn btn-primary btn-sm" href="editoriales_crear.php" title="Crear editorial">
-                             <i class="bi-plus-circle-fill"></i> Crear
-                            </a>
-                            <table class="table table-striped table-hover">
+            <div class='col-12'>
+                <div class='card'>
+                    <div class='card-header fs-3'>
+                        <i class=" fs-3 bi bi-book-half"></i> Editoriales
+                    </div>
+                    <div class='card-body'>
+                        <a class="btn float-end btn-lg" href="editoriales_crear.php" title="Crear editorial">
+                            <i class="bi-plus-circle-fill"></i> Crear
+                        </a>
+                        <table class="table  ">
                             <thead>
                                 <tr>
-                                    <th style="width=80%;">Editoriales</th>
-                                    <th style="width=80%;">Estatus</th>
-                                    <th style="width=20%;">&nbsp;</th>
-                                    <th style="width=20%;">&nbsp;</th>
+                                    <th>Editoriales</th>
+                                    <th>Estatus</th>
+                                    <th>&nbsp;</th>
+                                   
+
                                 </tr>
                             </thead>
                             <tbody>
@@ -44,15 +48,15 @@
                                 require_once './conexion.php';
                                 $sql = 'select id_editorial, nombre_editorial, estatus_editorial from editoriales order by nombre_editorial asc';
                                 $sentencia = $conexion->prepare($sql);
-                                $sentencia ->execute(); 
-                                foreach($sentencia->fetchAll(PDO::FETCH_ASSOC) as $editorial){
+                                $sentencia->execute();
+                                foreach ($sentencia->fetchAll(PDO::FETCH_ASSOC) as $editorial) {
                                     $editorial['nombre_editorial'] = htmlentities($editorial['nombre_editorial']);
                                     echo <<<fin
                                 <tr>
                                     <td>{$editorial['nombre_editorial']}</td>
                                     <td>{$editorial['estatus_editorial']}</td>
                                     <td>
-                                        <a class="btn btn-primary btn-sm" href="editoriales_editar.php?id_editorial={$editorial['id_editorial']}" title="Clic para editar categoria">
+                                        <a class="btn float-end btn-sm" href="editoriales_editar.php?id_editorial={$editorial['id_editorial']}" title="Clic para editar categoria">
                                             <i class="bi-pencil-square"></i> Editar
                                         </a>
                                     </td>
@@ -67,6 +71,7 @@ fin;
             </div>
         </div>
     </div>
-<script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/jquery-3.6.0.min.js"></script>
 </body>
+
 </html>
